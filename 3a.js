d20945008b8bb4369a317b9df625db4300aed90d
unknown <brucelukyanov@gmail.com>
2024-09-27 11:36:19 +0000

    3d added

--- 3a.js
+++ 3a.js
@@ -1,11 +1,11 @@
 //3a
-let tasks = [];
+let tasks = [];// Create an array of strings
 
 //3b
 let addTask = (task) => {
-    tasks.push(task);
-    console.log("Task added " + task)
-    return tasks.length;
+    tasks.push(task); // Add the task to the array
+    console.log("Task added " + task) // Print insertion message
+    return tasks.length; // Return the number of elements in the array
 }
 //3c
 let listAllTasks = ()=> {
@@ -16,7 +16,21 @@ let listAllTasks = ()=> {
 
  }; 
 
+ //3d
+ let deleteTask = (task)=> {
+    let index = tasks.indexOf(task);
+    if (index > -1) {
+        tasks.splice(index, 1); // Remove the task from the array
+        console.log("Task deleted" + task); // Print deletion message
+    } else {
+        console.log("Task not found"); // Task not found
+    }
+    return tasks.length; // Return the number of elements in the array
+};
 
 addTask("Work");
+addTask("Eat");
 listAllTasks();
+deleteTask("Work");
+
 
