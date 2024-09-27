2c0d9fc2642cb0cc4ce41964bf3ec0d8fd15c499
unknown <brucelukyanov@gmail.com>
2024-09-27 11:18:49 +0000

    3b added

--- 3a.js
+++ 3a.js
@@ -1,6 +1,15 @@
 //3a
 let tasks = [];
 
-//3
+//3b
+let addTask = (task) => {
+    tasks.push(task);
+    console.log("Task added " + task)
+    return tasks.length;
+}
+//3c
+let listAllTasks = ()=> {
+    
+}
 
-
+addTask("Work");
